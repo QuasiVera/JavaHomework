f5b8a0c34f200b5e2f63353e85dd09dadc40c8fd
VeraBu <opilki-in-yhe-head@mail.ru>
2023-03-28 00:34:26 +0300

    added Homework 1 1-3

---
+++ Homework1/Tasks1.java
@@ -0,0 +1,95 @@
+package Homework1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Tasks1 {
+    public static void main(String[] args) {
+        Task3();
+        }
+    
+        static void Task1() {
+        /*
+        Вычислить n-ое треугольное число (сумма чисел от 1 до n)
+         */
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите число n: ");
+            int n = in.nextInt();
+            in.close();
+            int sum = 0;
+            if (n<0) {  System.out.println("Введено отрицательное число");}
+            else if (n == 0 || n == 1) {  System.out.printf("Cумма равна: %d", n);}
+            else {
+            for (int i = 1; i <= n; i++)
+            {
+                sum+=i;
+            }
+            System.out.printf("Cумма равна: %d", sum);
+            }
+        }
+    
+        static void Task2() {
+    
+            /*
+            Вычислить n! (произведение чисел от 1 до n)
+             */
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите число n: ");
+            int n = in.nextInt();
+            in.close();
+            int factorial = 1;
+            if (n<0) {  System.out.println("Введено отрицательное число");}
+            else if (n == 0 || n == 1) {  System.out.printf("%d! равен 1", n);}
+            else
+            {
+            for (int i = 2; i <= n; i++)
+            {
+                factorial*=i;
+            }
+            System.out.printf("%d! равен %d", n, factorial);
+            }
+        }
+    
+        static void Task3() {
+    
+            /*
+            Вывести все простые числа от 1 до 1000 (числа, которые делятся только на 1 и на себя без остатка)
+             */
+            int n = 1000;
+            int[] arr = IntStream.rangeClosed(0, n).toArray();
+            arr[1] =0;  // Вычеркиваем 1, потому что 1 не считается простым числом
+            double sqrt = Math.sqrt(n);
+            for (int i = 2; i <=sqrt; i++)
+            {
+                for (int j = i*i; j <arr.length ; j=j+i)  // Вычеркиваем из массива числа кратные i начиная с i^2
+                {
+                    arr[j]=0;
+                }
+            }
+            int count =0;                       // 66-82 Убираем нули из массива через создание нового
+            for (int i = 0; i <=n ; i++)
+            {
+                if (arr[i]!=0)
+                {
+                    count++;
+                }
+            }
+            int[] newArr = new int[count];
+            int j =0;
+            for (int i = 0; i <=n ; i++)
+            {
+                if (arr[i]!=0){
+                newArr[j]=arr[i];
+                j++;
+                }
+            }
+    
+            String res = Arrays.toString(newArr);
+            System.out.printf(res);
+    
+    
+    
+    
+        }
+}
