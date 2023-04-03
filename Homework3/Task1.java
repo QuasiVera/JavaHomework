4ca6f7a937a4aeac681438b52963265319776385
VeraBu <Opilki-in-the-head@mail.ru>
2023-04-03 14:52:51 +0300

    added Homework3 1-2

---
+++ Homework3/Task1.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task1 {
+
+    /*
+     * Пусть дан произвольный список целых чисел, удалить из него четные числа
+     */
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+        int size = rnd.nextInt(20);
+       
+        ArrayList<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            int rand_item = rnd.nextInt(11);
+            list.add(rand_item);
+        }
+        
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
+                i--;
+            }
+        }
+        
+        System.out.printf("Результат: \n %s", list);
+
+    }
+}
---
+++ Homework3/Task2.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task2 {
+    /*
+     * Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка
+     */
+    public static void main(String[] args) {
+        
+        Random rnd = new Random();
+        int size = rnd.nextInt(20);
+       
+        ArrayList<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            int rand_item = rnd.nextInt(21);
+            list.add(rand_item);
+        }
+        
+        System.out.println(list);
+
+        int max = list.get(0);
+        int min = list.get(0);
+        int sum = list.get(0);
+
+        for (int i = 1; i < list.size(); i++) {
+            sum += list.get(i);
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+            if (list.get(i) < min) {
+                min = list.get(i);
+            }
+        }
+
+        double average = (double) sum / list.size();
+        System.out.printf("Max = %d, min = %d, Среднее арифметическое = %f", max,min,average);
+    }
+    
+}
