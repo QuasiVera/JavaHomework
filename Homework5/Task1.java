3e87870192b6bb0202b9a22d0f1cf408103cdf0c
VeraBu <Opilki-in-the-head@mail.ru>
2023-04-11 15:27:29 +0300

    corrected homework5

--- Homework5/Task1.java
+++ Homework5/Task1.java
@@ -1,7 +1,9 @@
 
 
 import java.util.*;
-
+/*
+ * 1. Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов
+ */
 
 public class Task1 {
     public static void main(String[] args) {
--- Homework5/Task_2.java
+++ Homework5/Task_2.java
@@ -1,5 +1,13 @@
 import java.util.*;
 
+/*
+ * 2. Пусть дан список сотрудников: Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин,
+Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина,
+Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.
+Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
+Для сортиорвки использовать TreeMap.
+ */
+
 public class Task_2 {
     public static void main(String[] args) {
         String userInput = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, \n" +
@@ -8,7 +16,7 @@ public class Task_2 {
 
         List<String> inputList = new ArrayList<String>(Arrays.asList(userInput.split("\\s*(,|\n)\\s*")));
         HashMap<String, Integer> names = new HashMap<>();
-
+        TreeMap<Integer, List<String>> sortedNames1 = new TreeMap<>(Comparator.reverseOrder());
         for (String person : inputList) {
             String temp = person.split(" ")[0];
             if (names.containsKey(temp)) {
@@ -17,9 +25,21 @@ public class Task_2 {
                 names.put(temp, 1);
             }
         }
+
+        for (var item : names.entrySet()) {
+            if (sortedNames1.containsKey(item.getValue())) {
+                List<String> list = sortedNames1.get(item.getValue());
+                list.add(item.getKey());
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(item.getKey());
+                sortedNames1.put(item.getValue(), list);
+            }
+        }
+
         // System.out.println(names);
-        List<Map.Entry<String, Integer>> sortedNames = new ArrayList<>(names.entrySet());
-        sortedNames.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        System.out.println(sortedNames);
+        // List<Map.Entry<String, Integer>> sortedNames = new ArrayList<>(names.entrySet());
+        // sortedNames.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));              * Оставила для себя
+        System.out.println(sortedNames1);
     }
 }
